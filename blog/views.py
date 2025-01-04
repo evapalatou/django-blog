ac188547a6550c193447083c8a7873ee607374e6
Evanthia Palatou <p7dt68xx@students.codeinstitute.net>
2025-01-04 12:16:46 +0000

    added comment CRUD

--- blog/views.py
+++ blog/views.py
@@ -57,24 +57,40 @@ def post_detail(request, slug):
         },
     )
 
-    def comment_edit(request, slug, comment_id):
+def comment_edit(request, slug, comment_id):
         """
         view to edit comments
         """
-    if request.method == "POST":
+        if request.method == "POST":
 
-        queryset = Post.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
-        comment = get_object_or_404(Comment, pk=comment_id)
-        comment_form = CommentForm(data=request.POST, instance=comment)
+            queryset = Post.objects.filter(status=1)
+            post = get_object_or_404(queryset, slug=slug)
+            comment = get_object_or_404(Comment, pk=comment_id)
+            comment_form = CommentForm(data=request.POST, instance=comment)
 
-        if comment_form.is_valid() and comment.author == request.user:
-            comment = comment_form.save(commit=False)
-            comment.post = post
-            comment.approved = False
-            comment.save()
-            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
-        else:
-            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+            if comment_form.is_valid() and comment.author == request.user:
+                comment = comment_form.save(commit=False)
+                comment.post = post
+                comment.approved = False
+                comment.save()
+                messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+            else:
+                messages.add_message(request, messages.ERROR, 'Error updating comment!')
 
-    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+def comment_delete(request, slug, comment_id):
+    """
+    view to delete comment
+    """
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if comment.author == request.user:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+    else:
+        messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
+
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))        
