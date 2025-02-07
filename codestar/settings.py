4a13d3baf16f5d568e8c997d7c42075f484617c0
Evanthia <epalatou@outlook.com>
2025-02-07 19:23:51 +0200

    update settings.py file django blog migration

--- codestar/settings.py
+++ codestar/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-evapalatou-djangoblog-kk04akz5omc.ws.codeinstitute-ide.net',
+ALLOWED_HOSTS = ['127.0.0.1',
                  '.herokuapp.com',]
 
 
